6ad9d65689d98c4dd19291ab73612b73cba9b1e1
CChiz <1657779419@qq.com>
2018-01-02 17:07:28 +0800

    我的第一次实训上传

---
+++ 我的第一次实训上传/175/src/Demo/Demo.java
@@ -0,0 +1,26 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class Demo {
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);	
+	System.out.println("请输入一个数");
+	int a=sc.nextInt();
+	Ac(a);
+	
+	}
+    
+	public static void Ac(int b){
+		for(int i = 0;i<b;i++){
+		System.out.println(i);	
+			
+			
+		}
+				
+		
+		
+		
+		
+	}
+}
